efb16e51ade37898778cd9db8ba1d86fb09c9fcb
Subba <kvsubbareddy238@gmail.com>
2026-01-11 21:47:31 +0530

    python basics

---
+++ Datatypes.py
@@ -0,0 +1,23 @@
+a=10
+print(type(a))
+b=10.5
+print(int(b))
+print(float(b))
+
+c="testing test    "
+print(type(c))
+print(c.upper())
+print(c.lower())
+print(c.split("t"))
+print(c.strip())
+print(c.count("u"))
+print(c.removeprefix("t"))
+print(c.replace("test","done"))
+
+
+
+d=[1,2,3,4,5]
+print(type(d))
+print(d)
+print(d.append('6'))
+
