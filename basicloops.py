324ff61beb36bcaffb293657f4714fb02db65395
Subba <kvsubbareddy238@gmail.com>
2026-01-12 22:11:40 +0530

    python basics

---
+++ basicloops.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)
+
+total = 0
+
+for i in range(1, 6):
+    total += i
+
+print("Sum is:", total)
