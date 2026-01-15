015055c311307fad1fd4950b0ababff9b0cc1aee
Subba <kvsubbareddy238@gmail.com>
2026-01-15 21:38:43 +0530

    python basics

--- practic2.py
+++ practic2.py
@@ -11,3 +11,21 @@ print(a)
 print(min(a))
 print(max(a))
 print(a is b)
+
+
+c={1:"apple",2:"banana",3:"cherry"}
+print(c)
+print(c[1])
+c.update({4:"orange"})
+print(c)
+c.pop(2)
+print(c)
+
+
+for i in c.keys():
+    print(i)
+
+for i in c.values():
+    print(i)
+
+
